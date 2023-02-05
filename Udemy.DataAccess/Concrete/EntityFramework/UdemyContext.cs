ebea56d663997a3e85cce7918582c3c53d3796ad
Sadi_er59 <Sadi_er59@itstep.edu.az>
2023-02-05 12:43:47 -0800

    Add project files.

---
+++ Udemy.DataAccess/Concrete/EntityFramework/UdemyContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Entity.Concrete;
+
+namespace Udemy.DataAccess.Concrete.EntityFramework
+{
+    public class UdemyContext : DbContext
+    {
+        public UdemyContext(DbContextOptions<UdemyContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<ObjectiveAndOutcomes> Objectives { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Requirement> Requirements { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
