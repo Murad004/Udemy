ae7f38b09e59130fc112dddd7b2f31ba296a337f
Sadi_er59 <Sadi_er59@itstep.edu.az>
2023-02-11 07:22:01 -0800

    ddd

--- Udemy.WebUI/Controllers/HomeController.cs
+++ Udemy.WebUI/Controllers/HomeController.cs
@@ -7,15 +7,21 @@ namespace Udemy.WebUI.Controllers
     public class HomeController : Controller
     {
         private ICategoryService _service;
+        private ISubCategoryService _subCategoryService;
+        private ITopicService _topicService;
+        private ICourseService _courseService;
 
-        public HomeController(ICategoryService service)
+        public HomeController(ICategoryService service, ISubCategoryService subCategoryService, ITopicService topicService, ICourseService courseService)
         {
             _service = service;
+            _subCategoryService = subCategoryService;
+            _topicService = topicService;
+            _courseService = courseService;
         }
 
         public IActionResult Index()
         {
-            return View(new LayoutViewModel { Categories=_service.GetAll()});
+            return View(new LayoutViewModel { Categories=_service.GetAll(),SubCategories=_subCategoryService.GetAll(),Topics=_topicService.GetAll(),Courses=_courseService.GetAll()});
         }
 
 
